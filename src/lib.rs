91328d0826a2bdca2fda6e8ed2a842a812160231
Shogo <55tetosan@gmail.com>
2020-05-24 13:24:37 +0900

    add feature

---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+use regex::Regex;
+
+pub trait MatcherTrait {
+    fn execute(&self, line: &str) -> bool;
+}
+
+pub struct FixedStringsMatcher {
+    pattern: String,
+}
+
+impl FixedStringsMatcher {
+    pub fn new(pattern: String) -> FixedStringsMatcher {
+        FixedStringsMatcher { pattern: pattern }
+    }
+}
+
+impl MatcherTrait for FixedStringsMatcher {
+    fn execute(&self, line: &str) -> bool {
+        line.contains(&self.pattern)
+    }
+}
+
+pub struct ExtendedRegexpMatcher {
+    pattern: Regex,
+}
+
+impl ExtendedRegexpMatcher {
+    pub fn new(pattern: String) -> ExtendedRegexpMatcher {
+        ExtendedRegexpMatcher {
+            pattern: Regex::new(&pattern).unwrap(),
+        }
+    }
+}
+
+pub enum Matcher {
+    ExtendedRegexp(ExtendedRegexpMatcher),
+    FixedStrings(FixedStringsMatcher),
+}
+
+impl Matcher {
+    pub fn new(pattern: String, is_fixed_strings_mode: bool) -> Matcher {
+        if is_fixed_strings_mode {
+            Matcher::FixedStrings(FixedStringsMatcher::new(pattern.to_string()))
+        } else {
+            Matcher::ExtendedRegexp(ExtendedRegexpMatcher::new(pattern.to_string()))
+        }
+    }
+
+    pub fn execute(&self, line: &str) -> bool {
+        match self {
+            Matcher::FixedStrings(m) => m.execute(line),
+            Matcher::ExtendedRegexp(m) => m.execute(line),
+        }
+    }
+}
+
+
+impl MatcherTrait for ExtendedRegexpMatcher {
+    fn execute(&self, line: &str) -> bool {
+        self.pattern.is_match(line)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_extended_regexp_matcher(){
+        let matcher = Matcher::new("Z".to_string(), false);
+        assert_eq!(false, matcher.execute("abcdefg"));
+        let matcher = Matcher::new("Z".to_string(), true);
+        assert_eq!(false, matcher.execute("abcdefg"));
+        let matcher = Matcher::new("a+.b+".to_string(), true);
+        assert_eq!(false, matcher.execute("aaa bbb"));
+    }
+}
