f6b1924466103fea1b075f6e6243710ee440eb44
Shogo <55tetosan@gmail.com>
2020-05-24 15:07:48 +0900

    add feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,38 @@
 use regex::Regex;
 
-pub trait MatcherTrait {
+trait MatcherTrait {
     fn execute(&self, line: &str) -> bool;
 }
 
-
-#[derive(Clone)]
-pub struct FixedStringsMatcher {
-    pattern: String,
-}
-
-impl FixedStringsMatcher {
-    pub fn new(pattern: String) -> FixedStringsMatcher {
-        FixedStringsMatcher { pattern: pattern }
-    }
-}
-
-impl MatcherTrait for FixedStringsMatcher {
-    fn execute(&self, line: &str) -> bool {
-        line.contains(&self.pattern)
-    }
-}
 #[derive(Clone)]
 pub struct ExtendedRegexpMatcher {
     pattern: Regex,
 }
-
 impl ExtendedRegexpMatcher {
     pub fn new(pattern: String) -> ExtendedRegexpMatcher {
         ExtendedRegexpMatcher {
             pattern: Regex::new(&pattern).unwrap(),
         }
+    }
+}
+impl MatcherTrait for ExtendedRegexpMatcher {
+    fn execute(&self, line: &str) -> bool {
+        self.pattern.is_match(line)
+    }
+}
+
+#[derive(Clone)]
+pub struct FixedStringsMatcher {
+    pattern: String,
+}
+impl FixedStringsMatcher {
+    pub fn new(pattern: String) -> FixedStringsMatcher {
+        FixedStringsMatcher { pattern: pattern }
+    }
+}
+impl MatcherTrait for FixedStringsMatcher {
+    fn execute(&self, line: &str) -> bool {
+        line.contains(&self.pattern)
     }
 }
 
@@ -39,7 +41,6 @@ pub enum Matcher {
     ExtendedRegexp(ExtendedRegexpMatcher),
     FixedStrings(FixedStringsMatcher),
 }
-
 impl Matcher {
     pub fn new(pattern: String, is_fixed_strings_mode: bool) -> Matcher {
         if is_fixed_strings_mode {
@@ -48,7 +49,6 @@ impl Matcher {
             Matcher::ExtendedRegexp(ExtendedRegexpMatcher::new(pattern.to_string()))
         }
     }
-
     pub fn execute(&self, line: &str) -> bool {
         match self {
             Matcher::FixedStrings(m) => m.execute(line),
@@ -57,24 +57,40 @@ impl Matcher {
     }
 }
 
-
-impl MatcherTrait for ExtendedRegexpMatcher {
-    fn execute(&self, line: &str) -> bool {
-        self.pattern.is_match(line)
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_extended_regexp_matcher(){
+    fn test_extended_regexp_matcher() {
+        let matcher = Matcher::new("c".to_string(), false);
+        assert_eq!(true, matcher.execute("abcdefg"));
+        let matcher = Matcher::new("fg".to_string(), false);
+        assert_eq!(true, matcher.execute("abcdefg"));
         let matcher = Matcher::new("Z".to_string(), false);
         assert_eq!(false, matcher.execute("abcdefg"));
+        let matcher = Matcher::new("a.c".to_string(), false);
+        assert_eq!(true, matcher.execute("abcdefg"));
+        let matcher = Matcher::new("a+.b+".to_string(), false);
+        assert_eq!(true, matcher.execute("aaa bbb"));
+        let matcher = Matcher::new("[aA][bB][cC]".to_string(), false);
+        assert_eq!(true, matcher.execute("aBc"));
+        assert_eq!(true, matcher.execute("Abc"));
+    }
+    #[test]
+    fn test_match_fix_string() {
+        let matcher = Matcher::new("c".to_string(), true);
+        assert_eq!(true, matcher.execute("abcdefg"));
+        assert_eq!(true, matcher.execute("cccc"));
+        let matcher = Matcher::new("fg".to_string(), true);
+        assert_eq!(true, matcher.execute("abcdefg"));
         let matcher = Matcher::new("Z".to_string(), true);
+        assert_eq!(false, matcher.execute("abcdefg"));
+        let matcher = Matcher::new("a.c".to_string(), true);
         assert_eq!(false, matcher.execute("abcdefg"));
         let matcher = Matcher::new("a+.b+".to_string(), true);
         assert_eq!(false, matcher.execute("aaa bbb"));
+        let matcher = Matcher::new("[aA][bB][cC]".to_string(), true);
+        assert_eq!(false, matcher.execute("aBc"));
     }
 }
